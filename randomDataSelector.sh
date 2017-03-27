9b2df3e69b6d46b5b4da725fe0b863fa935e6c6c
Dimitris Galanis <dimitris@nlpbroker.ilsp.gr>
2017-03-27 13:57:22 +0300

    added randomDataSelector.sh

---
+++ randomDataSelector.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+INPUTDIR=$1
+OUTPUTDIR=$2
+NUMOFFILES=$3
+
+printUsage(){
+ echo "Usage: randomDataSelector.sh <INPUTDIR> <OUTPUTDIR> <NUMOFFILES>"
+}
+
+if [ "$#" -ne  "3" ]
+then
+	printUsage
+	exit
+else
+	echo "-- --- -- --- --"
+	# Remove OUTPUTDIR and all its contents
+	if [ -d "$OUTPUTDIR" ]; then
+        	echo "Remove OUTPUTDIR: $OUTPUTDIR"
+	        rm -rf $OUTPUTDIR
+	fi
+
+	# Create OUTPUTDIR
+	echo "Creating OUTPUTDIR: $OUTPUTDIR"
+	mkdir $OUTPUTDIR
+
+	# Selecting data.
+	echo "Randomly selecting data from: $INPUTDIR" 
+	find $INPUTDIR -type f | shuf -n $NUMOFFILES | xargs cp -t $OUTPUTDIR
+	
+	# ls
+	echo "Contents of output dir: $OUTPUTDIR"
+	ls -l $OUTPUTDIR
+fi
+
+
